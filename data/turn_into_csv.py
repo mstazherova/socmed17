c44602320d742edfb177b7ef43cbea824e95a0f4
Maximilian Seidler <maxseidler@gmx.net>
2017-08-24 21:18:39 +0200

    data preprocessing - work in progress

---
+++ data/turn_into_csv.py
@@ -0,0 +1,23 @@
+import re
+
+tweets = []
+
+with open('sample_tweets.txt') as fp:
+    for line in fp:
+        tweets.append(line)
+
+for element in tweets:
+    # find 18 digit user_id...
+    user_id = re.findall(r"(\d{18})", element)
+    # to filter out date and user_id
+    if len(user_id) > 0:
+        _, _, tweet = element.partition(user_id[0] + "\",")
+    else:
+        tweet = element
+    # filter out user mentions
+    tweet = re.sub("(?<=^|(?<=[^a-zA-Z0-9-\.]))@([A-Za-z_]+[A-Za-z0-9_]+)", '', tweet)
+    # filter out re-tweet indicatiors
+    tweet = re.sub("RT : ", '', tweet)
+    # filter out links
+    tweet = re.sub("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", '', tweet)
+    print(tweet)
