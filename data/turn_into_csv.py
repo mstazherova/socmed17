2a8974f94f19e74e769d5ec68e30a7748855b547
Maximilian Seidler <maxseidler@gmx.net>
2017-09-09 18:50:49 +0300

    remove tweets without target emojis and added docstrings

--- data/turn_into_csv.py
+++ data/turn_into_csv.py
@@ -1,33 +1,53 @@
 import re
 import emoji
 
-# reading in the tweets and putting them into an array
-tweets = []
-with open('sample_tweets.txt', encoding='UTF-8') as fp:
-    current = ''
-    for line in fp:
-        # if a user id is found...
-        if re.findall(r"(\d{18})", line):
-            # ... we are at the beginning of a new tweet
-            previous = current
-            current = line
-            tweets.append(previous)
-        # if not, then the line is still part of the current tweet
-        else:
-            current += line
-# the first element is now an empty string so it gets removed
-tweets = tweets[1:]
+
+def contains_target_emoji(tweet):
+    """Returns True if at least one of the target emojis appears in a tweet.
+    Returns False otherwise."""
+    emojis = set(c for c in tweet if c in emoji.UNICODE_EMOJI)
+    targets = {'😨', '😱', '😍', '❤', '😳', '😮', '😡', '😠', '😢', '😔',
+               '😖', '🤢'}
+    return len(emojis & targets) > 0
+
+
+def make_tweet_list(filepath):
+    """Given a file-path this function identifies the the boundaries of a tweet
+    and makes an array with each element being a tweet."""
+
+    tweets = []
+    with open(filepath, encoding='UTF-8') as fp:
+        current = ''
+        for line in fp:
+            # if a user id is found...
+            if re.findall(r"(\d{18})", line):
+                # ...we are at the beginning of a new tweet
+                previous = current
+                current = line
+                if contains_target_emoji(previous):
+                    tweets.append(previous)
+            # if not, then the line is still part of the current tweet
+            else:
+                current += line
+    # element 0 is an empty string, so it doesn't need to be returned
+    return tweets[1:]
 
 
 def extract_emojis(tweet):
+    """Searches for emoji characters in a string and removes them.
+    Returns a (string, emojis) tuple."""
+
     emojis = list(c for c in tweet if c in emoji.UNICODE_EMOJI)
     for emoji_ in emojis:
-        tweet = tweet.replace(emoji_, "")
+        tweet = tweet.replace(emoji_ + ' ', '')
 
     return (tweet, emojis)
 
 
 def identify_emotions(emojis):
+    """Given a list of emojis it identifies the corresponding emotion.
+    Returns a list of strings."""
+
     emotions = []
     for emoji_ in set(emojis):
         if emoji_ == '😨' or emoji_ == '😱':
@@ -45,24 +65,51 @@ def identify_emotions(emojis):
     return emotions
 
 
-for tweet in tweets:
+def cleaned_up(tweet):
+    """Given a list of tweets this function does some preprocessing by removing
+    noise.
+
+    Example:
+        "2017-08-16 19:10:26","897898364787978241","RT @tushy_com: RT if you
+        wouldn't mind being stuck in between @rileyreidx3 &amp; @AidraFOfficial!
+        😍 #tushy https://t.co/mkUvdIvyeW"
+
+        becomes:
+
+        "RT if you wouldn't mind being stuck in between 😍 tushy"
+    """
+
     # find 18 digit user_id...
     user_id = re.findall(r"(\d{18})", tweet)
     # to filter out date and user_id
     _, _, tweet = tweet.partition(user_id[0] + "\",")
     # filter out user mentions
-    tweet = re.sub("(?<=^|(?<=[^a-zA-Z0-9-\.]))@([A-Za-z_]+[A-Za-z0-9_]+)", '', tweet)
+    tweet = re.sub("(?<=^|(?<=[^a-zA-Z0-9-\.]))@([A-Za-z_]+[A-Za-z0-9_]+)", "", tweet)
     # filter out re-tweet indicatiors
     tweet = re.sub("RT : ", '', tweet)
     # filter out links
     tweet = re.sub("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", '', tweet)
+    # remove '#' form hashtags
+    tweet = re.sub("#", '', tweet)
+    # remove '&amp';
+    tweet = re.sub("&amp;", '', tweet)
     # filter out non-ascii characters but leaving emojis
-    emoji_pat = '[\U0001F300-\U0001F64F\U0001F680-\U0001F6FF\u2600-\u26FF\u2700-\u27BF]'
+    emoji_pat = '[\U0001F300-\U0001F64F\U0001F680 - \U0001F6FF\u2600-\u26FF\u2700-\u27BF]'
     reg = re.compile(r'({})|[^\x00-\x7F]'.format(emoji_pat))  # from non-ascii chars return emojis
     tweet = reg.sub(lambda x: '{}'.format(x.group(1)) if x.group(1) else '', tweet)
-    # remove newline at the end of tweet
-    tweet = tweet[:-1]
+    # remove beginning and ending quotations marks
+    tweet = tweet[1:-2]
+    # remove extra whitespace
+    tweet = ' '.join(tweet.split())
+    # remove newline at the end of tweet and return clean tweet
+    return tweet
 
-    tweet_plus_emojis = extract_emojis(tweet)
-    print(tweet_plus_emojis)
-    print(identify_emotions(tweet_plus_emojis[1]))
+
+tweets = make_tweet_list('sample_tweets.txt')
+for tweet in tweets:
+    #print(tweet)
+    clean_tweet, emojis = extract_emojis(cleaned_up(tweet))
+    emotions = identify_emotions(emojis)
+    #print(clean_tweet)
+    #print(emotions)
+
