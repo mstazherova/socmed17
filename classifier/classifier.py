b8aa915db6f29e66f1b98aadf3a4822c3dae2322
Maximilian Seidler <maxseidler@gmx.net>
2017-09-14 16:49:16 +0200

    Further testing and code clean up

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,6 +1,6 @@
 # Python 3.6
 # Authors: Maximilian Seidler, Mariia Stazherova
-# Reads CSV-file and performs various classifications
+# Reads CSV-file and performs various sentiment classifications
 
 import csv
 import time
@@ -10,12 +10,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import VotingClassifier
+
+
+# Used classification algorithms for sentiment analysis
+clf_list = [MultinomialNB(),
+            LinearSVC(),
+            LogisticRegression(random_state=42),
+            RandomForestClassifier(criterion='entropy', n_estimators=5)
+            ]
 
 
 def read_data(filepath):
@@ -31,51 +40,85 @@ def read_data(filepath):
             y.append(row[0])
             X.append(row[1:][0])
             counter += 1
-            if debug and counter == 10000:
+            # limit data for testing
+            if debug and counter == 20000:
                 break
     return (X, y)
 
 
-def print_emotion_frequency(y):
-    c = Counter(y)
-    print(c.most_common())
-
-
-def classifiers(X_train, y_train, X_test, y_test, classifiers):
-    for classifier in classifiers:
-        start_time = time.time()
-        clf = classifier
-        clf.fit(X_train, y_train)
-        print("{} completed in {:.2f} seconds".format(type(clf).__name__, (time.time() - start_time)))
-        # print("Cross-validated accuracy: {:.2f}".format(cross_val_score(clf, X_test, y_test, scoring='accuracy').mean()))
-        print("Accuracy: {}".format(clf.score(X_test, y_test)))
-        print()
-
-
-if __name__ == "__main__":
-    X, y = read_data('../data/corpus.csv')
-    # vectorization
+def vectorize_data(X):
+    """Given a list of sentences, this function returns a matrix of vectorized
+    term frequencies."""
     vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 2))
     X_vec_counts = vectorizer.fit_transform(X)
     # from counts to term frequencies
     # (idf is unneccesary as we have only one document class)
     tfidf_transformer = TfidfTransformer(use_idf=False)
-    X_vec_freq = tfidf_transformer.fit_transform(X_vec_counts)
-    # split into train and test set
-    X_train, X_test, y_train, y_test = train_test_split(X_vec_freq, y, test_size=0.2, random_state=42)
-    print("Data reading complete!")
-    print_emotion_frequency(y)
-    # classfication
-    clf_list = [MultinomialNB(),
-                LinearSVC(),
-                DecisionTreeClassifier(criterion='entropy', random_state=42),
-                RandomForestClassifier(criterion='entropy', n_estimators=3),
-                LogisticRegression(random_state=42)]
+    return tfidf_transformer.fit_transform(X_vec_counts)
 
+
+def print_emotion_ratio(y):
+    """This funciton prints the frequency and ratio of each of the labels
+    (emotions) in the data."""
+    c = Counter(y)
+    print("Label distribution in the data:")
+    for emotion, frequency in c.most_common():
+        print("\t{}: {} ({:.2f}%)".format(emotion, frequency, frequency / len(y)))
+    print()
+
+
+def essemble_classifier():
+    """This function collects the names of the classifiers and passes them to
+    the Voting Classifier. For consistency reasons, it returns a list with a
+    single element (the VotingClassifier)."""
     clf_names = []
     for clf in clf_list:
         clf_names.append(type(clf).__name__)
-    voting = [VotingClassifier(list(zip(clf_names, clf_list)))]
+    # the VotingClassifier expects a list of tuples in the format of
+    # (name_of_classifier, classifier)
+    return [VotingClassifier(list(zip(clf_names, clf_list)))]
 
-    classifiers(X_train, y_train, X_test, y_test, clf_list)
-    classifiers(X_train, y_train, X_test, y_test, voting)
+
+def train_and_evaluate(X_train, y_train, X_test, y_test, classifiers):
+    """Given a list of classifiers, this function trains each classifier with a
+    training set and evaluates it with a test set."""
+    for clf in classifiers:
+        start_time = time.time()
+        clf.fit(X_train, y_train)
+        print("\tTraining of {} completed in {:.2f} seconds".format(type(clf).__name__, time.time() - start_time))
+        # print("AUC score: {}".format(roc_auc_score(y_test, clf.decision_function(X_test))))
+        print("\t\tAccuracy: {:.2f}".format(clf.score(X_test, y_test)))
+
+
+# def cross_validated_scores(X, y, classifiers):
+#     for clf in classifiers:
+#         start_time = time.time()
+#         print("{}".format(type(clf).__name__))
+#
+#         print("Cross-validated f1_weighted: {:.2f} (finised in {:.2f} seconds)"
+#               .format(cross_val_score(clf, X, y, cv=5, scoring='f1_weighted').mean(),
+#                       time.time() - start_time))
+#         print()
+
+
+if __name__ == "__main__":
+    print("Processing data...")
+    # read from corpus
+    X, y = read_data('../data/corpus.csv')
+    # vectorize data
+    X_vec = vectorize_data(X)
+    # split into train and test set (by enabling stratification we ensure that
+    # the train and test sets have the same ratio of emotions)
+    X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.1,
+                                                        random_state=42, stratify=y)
+
+    print("Data processing completed.")
+    print_emotion_ratio(y)
+    print("Training and evaluating classifiers:")
+    # train and evaluate each of the classifiers
+    train_and_evaluate(X_train, y_train, X_test, y_test, clf_list)
+    # train and evaluate with an essemble method (using all of the classifiers)
+    train_and_evaluate(X_train, y_train, X_test, y_test, essemble_classifier())
+
+    # cross_validated_scores(X, y, clf_list)
+    # cross_validated_scores(X, y, essemble_classifier())
