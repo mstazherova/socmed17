d50170bba2e04f43fdac8d9724035d228ca9b03a
Maria Stazherova <mstazherova@users.noreply.github.com>
2017-09-15 15:46:30 +0200

    removed authentication keys

--- streaming/config.py
+++ streaming/config.py
@@ -2,7 +2,7 @@
 # https://dev.twitter.com/oauth/overview/application-owner-access-tokens
 
 class TwitterAuth:
-    CONSUMER_KEY = 'Og9EHMrtpgHs8WJRZlsYA25rZ'
-    CONSUMER_SECRET = 'psdfATmllw8pnUJYineEEIYK1pJIdWAqUdCvasYNfVB6u7rtCg'
-    ACCESS_TOKEN = '199341516-qy6f8ddhTmv0GnfEOHvZ2xJOtoRxMCggQMm7JBDK'
-    ACCESS_TOKEN_SECRET = 'drGWTjdOXfQs0Q8ZcMYunKjJSnygQ0pYZlnTYwo6Kf4dq'
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    ACCESS_TOKEN = ''
+    ACCESS_TOKEN_SECRET = ''
